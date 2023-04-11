05c1a92ea06f8e5a6206b5a63c157c1356eb64cb
Marcelo Chaves <Marcelochaves20000@gmail.com>
2023-04-11 00:18:55 -0300

    finish course

--- alura-styled-components-master/src/Components/Conta/index.jsx
+++ alura-styled-components-master/src/Components/Conta/index.jsx
@@ -3,7 +3,7 @@ import privado from "../../assets/images/privado.svg";
 import olho_icone from "../../assets/images/olho.svg";
 import dinheiro from "../../assets/images/dinheiro.svg";
 import styled from "styled-components";
-import { Icone, Box, Button, Saldo, Detalhe } from "../UI";
+import { Icone, Box, Button, Saldo, Detalhe, IconeTema } from "../UI";
 
 const IconeMargin = styled(Icone)`
   margin-top: 2px;
@@ -22,7 +22,7 @@ const Conta = () => {
       <div style={{ fontSize: "26px", padding: "20px 0" }}>
         Saldo disponível{" "}
         <span>
-          <Icone src={dinheiro} alt="Ícone Saldo" />
+          <IconeTema src={dinheiro} alt="Ícone Saldo" />
         </span>
         {toggleState ? (
           <Saldo>
--- alura-styled-components-master/src/Components/ImageFilter.js
+++ alura-styled-components-master/src/Components/ImageFilter.js
@@ -4,15 +4,15 @@ import outros from '../assets/images/outros.svg';
 import saude from '../assets/images/saude.svg';
 import transporte from '../assets/images/transporte.svg';
 import utilidades from '../assets/images/utilidades.svg';
-import { Icone } from "./UI";
+import { IconeTema } from "./UI";
 
 export default (type) => {
   const Images = {
-    Restaurante: <Icone src={alimentacao} alt="Restaurante"/>,
-    Saude: <Icone src={saude} alt="saude"/>,
-    Transporte: <Icone src={transporte} alt="transporte"/>,
-    Utilidades: <Icone src={utilidades} alt="utilidades"/>,
-    default: <Icone src={outros} alt="outros"/>
+    Restaurante: <IconeTema src={alimentacao} alt="Restaurante"/>,
+    Saude: <IconeTema src={saude} alt="saude"/>,
+    Transporte: <IconeTema src={transporte} alt="transporte"/>,
+    Utilidades: <IconeTema src={utilidades} alt="utilidades"/>,
+    default: <IconeTema src={outros} alt="outros"/>
   }
 
   return Images[type] || Images.default;
